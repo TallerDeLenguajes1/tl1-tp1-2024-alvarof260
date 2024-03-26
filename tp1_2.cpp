ca08598144d8780d3181469dcbb1d3d6449ad6f7
alvaro <alvarof260@gmail.com>
2024-03-26 16:36:08 -0300

    commit initial

---
+++ tp1_2.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int cuadrado1(int n);
+void cuadrado(int *n);
+
+int main()
+{
+    printf("Ingrese un numero: ");
+    int n;
+    int *np;
+
+    scanf("%d", &n);
+
+    np = &n;
+
+    cuadrado(np);
+
+    printf("%d", n);
+
+    /* cuadrado = cuadrado1(n);
+
+    printf("El cuadrado del numero es: %d", cuadrado); */
+
+
+
+
+    return 0;
+}
+
+int cuadrado1(int n)
+{
+    int cuadrado;
+    cuadrado = n * n;
+
+    return cuadrado;
+}
+
+void cuadrado(int *n)
+{
+    *n *= *n;
+}
