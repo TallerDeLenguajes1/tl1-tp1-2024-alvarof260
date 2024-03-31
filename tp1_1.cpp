722e8486949c806c91b72d8d6188a8ce0fe2dc33
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-31 01:09:57 -0300

    feat: puntos 3 a, b, c, d, e y f

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,13 @@
 
 int main()
 {
-    std::cout << "Hola mundo!" << std::endl;
+    int number;
+    int *pint;
+    number = 5;
+    pint = &number;
+    std::cout << "el contenido del puntero es: " << *pint << std::endl;
+    std::cout << "la direccion de memoria almacenada en el puntero es: " << pint << std::endl;
+    std::cout << "la direccion de memoria de la variable es: " << &number << std::endl;
+    std::cout << "la direccion de memoria almacenada en el puntero es: " << &pint << std::endl;
+    std::cout << "el tamanio de memoria de la variable: " << sizeof(number) << std::endl;
 }
