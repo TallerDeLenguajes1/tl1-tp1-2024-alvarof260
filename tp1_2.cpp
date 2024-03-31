4b712ea3bafa995bb01bea776cfdd383005cc191
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-31 01:37:15 -0300

    feat: agregar funcion potencia con void

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <iostream>
 
-int potencia(int base, int exponente);
+// int potencia(int base, int exponente);
+void potencia(int base, int exponente);
 
 int main()
 {
     int numero;
-    int respuesta;
+    // int respuesta;
     std::cout << "Ingrese un numero entero: ";
     std::cin >> numero;
-    respuesta = potencia(numero, 2);
-    std::cout << "El numero ingresado es: " << respuesta << std::endl;
-
+    // respuesta = potencia(numero, 2);
+    // std::cout << "El numero elevado a 2 es: " << respuesta << std::endl;
+    potencia(numero, 2);
     return 0;
 }
-
+/*
 int potencia(int base, int exponente)
 {
     int resultado = 1;
@@ -23,4 +24,14 @@ int potencia(int base, int exponente)
         resultado *= base;
     }
     return resultado;
+} */
+
+void potencia(int base, int exponente)
+{
+    int resultado = 1;
+    for (int i = 0; i < exponente; i++)
+    {
+        resultado *= base;
+    }
+    std::cout << "El numero elevado a 2 es: " << resultado << std::endl;
 }
