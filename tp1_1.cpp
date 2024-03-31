24ee345bbd10beabcc3232115b9e47e2834f1dd5
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-31 00:40:51 -0300

    feat: agregar salida con string

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,16 +1,7 @@
 #include <stdio.h>
+#include <iostream>
 
 int main()
 {
-    int x;
-    printf("Hola mundo!");
-    scanf("%d", &x);
-    int *p;
-    p = &x;
-    printf("Contenido del puntero: %d\n",*p);
-    printf("Direccion de memoria %p\n", p);
-    printf("Direccion de memoria de la variable %p\n", &x);
-    printf("Direccion de memoria del puntero %p\n", &p);
-    printf("Tamaño de memoria: %d", sizeof(x));
-    return 0;
+    std::cout << "Hola mundo!" << std::endl;
 }
